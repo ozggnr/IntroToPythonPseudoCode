5fb3b33bce404a7c38b00d85f0eab86dbc4332f9
ozggnr <56851195+ozggnr@users.noreply.github.com>
2019-11-17 22:00:28 -0500

    Update newhom9.py

--- newhom9.py
+++ newhom9.py
@@ -1,11 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd 
-from sklearn import preprocessing
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
-from sklearn import model_selection, neighbors
+from sklearn import model_selection
 from sklearn.cluster import KMeans
 iris = load_iris()
 iternim = 10000
@@ -17,13 +15,15 @@ for i in range(0,iternim):
     model.fit(X_train,y_train)
     b = metrics.silhouette_score(X_test,y_test)
     if b >= 0.80:
-        print(b)
         trainselect.append(X_train)
         trainselect.append(y_train)
         print(trainselect)
+        print(b)
+        print('Threshold is satisfied!')
         break
     a.append(b)
     if i == iternim-1:
+        print('10000 iteration is passed and threshold is not satisfied. This is the best result!')
         print(max(a))
 
 
